98149680faed09fb6123fb9b1a3b8a9eb3c56fb3
rokas <rokasra@gmail.com>
2022-03-21 16:40:10 +0200

    pridejau del komentarus del aiskumo, LocalS works

--- js/caoCookie.js
+++ js/caoCookie.js
@@ -1,12 +1,15 @@
 const inputEl = document.getElementById("name");
-const h2El = document.getElementById("display");
+const ulEl = document.querySelector(".column");
 const formEl = document.querySelector("form");
 
 formEl.onsubmit = function (e) {
   e.preventDefault();
   let inputVal = formEl.elements.name.value.trim();
   console.log("inputVal===", inputVal);
+  addLi(inputVal);
   formEl.elements.name.value = " ";
   //cookies
   document.cookie = `Name=${inputVal}`;
 };
+
+const cookieNames = document.cookie;
