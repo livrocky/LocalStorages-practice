0f9afdc15ec1705b6068b31e0bd9486962f2e760
rokas <rokasra@gmail.com>
2022-03-21 16:39:44 +0200

    pridejau komentarus del aiskumo, localS works

--- js/localS.js
+++ js/localS.js
@@ -1,8 +1,10 @@
 const btnEl = document.getElementById("btn1");
 const h2El = document.querySelector("h2");
-const formEl = document.forms[0];
-const ulEl = document.querySelector(".names");
+//padarem kad issaugotu reiksme ir po perkrovimu
 let total = localStorage.getItem("total") === null ? 0 : localStorage.getItem("total");
+
+//VIENAS IS BUDU//
+
 // let total;
 //is storage
 
@@ -12,6 +14,9 @@ let total = localStorage.getItem("total") === null ? 0 : localStorage.getItem("t
 //   total = localStorage.getItem("total");
 // }
 
+//PASPAUDUS MYGTUKA SKAICIUS PADIDEJA +1
+//TADA ISIKELIA I LOCAL STORAGE
+
 h2El.textContent = total;
 btnEl.addEventListener("click", () => {
   h2El.textContent = ++total;
@@ -19,21 +24,65 @@ btnEl.addEventListener("click", () => {
   localStorage.setItem("total", total);
 });
 
+/* 2
+<form>
+  <input type="text" id="name" placeholder="Enter name" />
+</form>
+ivedus varda ir issiuntus forma su enter(automatiskai vysksta)
+pridedam ta varda i vardu sarasa, kaip saraso el
+*/
+
+const formEl = document.querySelector("form");
+const ulEl = document.querySelector(".names");
+const inputVal = document.getElementById("name");
+
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
-  const inpEl = formEl.elements.name;
-  //paimti reiksme
-  let inputVal = inpEl.value.trim();
-  console.log("inputVal===", inputVal);
-  addLi(inputVal);
-  inpEl.value = " ";
+  let inputValue = inputVal.value.trim();
+  console.log("inputValue===", inputValue);
+  //Idedam i ta sukurta funkcija savo input Value.
+  addLi(inputValue);
+  inputVal.value = " ";
 });
 
+//sukurti arr kad ideti i local storage
+const namesArr = [];
+//susikuriam funkcija kuri sukurs li ir duos text content.
 function addLi(stringName) {
-  //create li
   const liEl = document.createElement("li");
-  //add text
   liEl.textContent = stringName;
-  //place el
   ulEl.append(liEl);
+  //add to array [LOCAL STORAGE]//
+  namesArr.push(stringName);
+  saveToStorage();
 }
+
+function saveToStorage() {
+  const stringFromArr = namesArr.join(",");
+  localStorage.setItem("names", stringFromArr);
+}
+//[LOCAL STORAGE] ENDS//
+
+//TAS PATS SU MOKYTOJU DARYTA//
+
+// const formEl = document.forms[0];
+// const ulEl = document.querySelector(".names");
+
+// formEl.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const inpEl = formEl.elements.name;
+//   //paimti reiksme
+//   let inputVal = inpEl.value.trim();
+//   console.log("inputVal===", inputVal);
+//   addLi(inputVal);
+//   inpEl.value = " ";
+// });
+
+// function addLi(stringName) {
+//   //create li
+//   const liEl = document.createElement("li");
+//   //add text
+//   liEl.textContent = stringName;
+//   //place el
+//   ulEl.append(liEl);
+// }
